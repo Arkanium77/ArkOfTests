c77b42452807465a9133a693d1259729be77caa2
Arkanium77 <arkanium77@gmail.com>
2020-03-14 18:24:12 +0300

    Template of Asserts class

---
+++ src/main/java/ArkOfAsserts.java
@@ -0,0 +1,13 @@
+public class ArkOfAsserts {
+    public static boolean assertEquals(Object first, Object second) {
+        return true;
+    }
+
+    public static boolean assertTrue(Object object) {
+        return true;
+    }
+
+    public static boolean assertNotNull(Object object) {
+        return true;
+    }
+}
