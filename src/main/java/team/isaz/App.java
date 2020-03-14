1fe5ffbb1c6cb412ca42ec7d378b8f6db86ce9e9
Arkanium77 <arkanium77@gmail.com>
2020-03-14 18:03:12 +0300

    Initial commit

---
+++ src/main/java/team/isaz/App.java
@@ -0,0 +1,13 @@
+package team.isaz;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        ArkOfTests.execute("team.isaz.exampleClassesTest");
+    }
+}
---
+++ src/main/java/team/isaz/ArkOfTests.java
@@ -0,0 +1,7 @@
+package team.isaz;
+
+public class ArkOfTests {
+    public static void execute(String testingPackage) {
+        System.out.println( "Hello World!" );
+    }
+}
