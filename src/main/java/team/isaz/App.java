764a25551c5b3fa7f8c689511e0fb73e0b351b6b
Arkanium77 <arkanium77@gmail.com>
2020-03-16 13:26:32 +0300

    First version of framework is works

--- src/main/java/team/isaz/App.java
+++ src/main/java/team/isaz/App.java
@@ -1,13 +1,20 @@
 package team.isaz;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import team.isaz.exceptions.PackageNotFoundException;
+import team.isaz.framework.ArkOfTests;
+
+import java.io.IOException;
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        ArkOfTests.execute("team.isaz.exampleClassesTest");
+    private static Logger logger = LoggerFactory.getLogger(App.class);
+
+    public static void main( String[] args ) throws ClassNotFoundException, IOException, PackageNotFoundException {
+        ArkOfTests.execute("team.isaz.exampleClassesTests");
     }
 }
