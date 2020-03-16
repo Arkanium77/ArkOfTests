56a1044a43ee1f1240d943bf69be30181c1f7251
Arkanium77 <arkanium77@gmail.com>
2020-03-16 13:57:29 +0300

    Exception renamed

---
+++ src/main/java/team/isaz/exceptions/PackageAnalyzeException.java
@@ -0,0 +1,16 @@
+package team.isaz.exceptions;
+
+/**
+ * Исключение, возникающее в случае ошибок анализа пакета,
+ * например, если он не найден или
+ * произошла неудачная попытка загрузть класс ему принадлежащий.
+ */
+public class PackageAnalyzeException extends Exception {
+    public PackageAnalyzeException(String message){
+        super(message);
+    }
+
+    public PackageAnalyzeException() {
+        super();
+    }
+}
